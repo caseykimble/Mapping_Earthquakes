d5fc1e87cc3b8072a4257be69e59f15672f55156
caseykimble <105175961+caseykimble@users.noreply.github.com>
2022-12-05 15:57:08 -0700

    Upload Challenge files

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -181,7 +181,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 });
 
 
-// Here we create a legend control object.
+// Create a legend control object.
 let legend = L.control({
   position: "bottomright"
 });
@@ -210,13 +210,12 @@ legend.onAdd = function() {
     return div;
 };
 
-// Finally, we add our legend to the map.
+// Add our legend to the map.
 legend.addTo(map);
 
 // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
 d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
     
-  // Create a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
         color: "red",
         weight: 2 
